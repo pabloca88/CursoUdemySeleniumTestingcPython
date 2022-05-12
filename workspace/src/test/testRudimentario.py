a18d9fd939852955ccd8513e126f7c4110b64f09
pabloca88 <pabloca88@hotmail.com>
2022-05-12 19:17:41 -0300

    arreglando las carpetas del framework

--- workspace/src/test/testRudimentario.py
+++ workspace/src/test/testRudimentario.py
@@ -1,14 +1,32 @@
-from selenium.webdriver.common.keys import Keys
+# IMPORT LIBS
+import time
+
 from selenium import webdriver
+import time
 
+# INIZIALIZO EL DRIVER
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome("/Users/pablocalvano/Documents/Cursos Automation Udemy/CURSO AUTOMATION PYTHON "
-                          "MERVIN/CursoUdemySeleniumTestingcPython/workspace/Scripts/chromedriver")
+driver = webdriver.Chrome("//Users/pablocalvano/Documents/Cursos Automation Udemy/CURSO AUTOMATION PYTHON "
+                          "MERVIN/CursoUdemySeleniumTestingcPython/workspace/environment/Scripts/chromedriver")
+
+# VOY HOST DE LA APLICACION
 driver.get("http://www.python.org")
+
+# SE VERIFICA AL TITULO DE LA APLICACION
 assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+
+time.sleep(10)
+
+# ALMACENO EN UNA VARIABLE EL OBJETO CON EL QUE VOY A INTERACTUAR
+elem = driver.find_element_by_id("id-search-field")
+
 elem.clear()
+
 elem.send_keys("pycon")
+
 elem.send_keys(Keys.RETURN)
+
 assert "No results found." not in driver.page_source
+
 driver.close()
